a46cc6aa3c5ffcebd0108bf9bb7eef26f2ef72f5
dj <dj0@gmx.de>
2018-01-13 17:11:45 +0100

    Update L.mytrack.js

--- L.mytrack.js
+++ L.mytrack.js
@@ -1,14 +1,13 @@
 L.Mytrack = L.Layer.extend({
-options: {click:true},  //click or locationfound
+options: {click:false},  //click or locationfound
 lng0: 0,  //prevent duplikates
 gpx: {"type": "LineString","coordinates": []},
-initialize: function(options) {L.setOptions(this, options);},  //console.log(this.options)
+initialize: function(options) {L.setOptions(this, options);},
 onAdd: function(map) {this.myLayer=L.geoJSON().addTo(map); map.on(this.options.click?'click':'locationfound', this._onclick, this);},
 _onclick: function(e) {if(e.latlng.lng!=this.lng0) {this.lng0=e.latlng.lng; this.gpx.coordinates.push([e.latlng.lng,e.latlng.lat]); this.myLayer.addData(this.gpx); this._map.panTo([e.latlng.lat,e.latlng.lng])}}
 });
 
 L.mytrack = function(options) {return new L.Mytrack(options);}; //L.mytrack = new L.Mytrack();
- //console.log(L.mytrack)
 
 	L.Control.Watermark = L.Control.extend({  //download-button
 		onAdd: function(map) {
@@ -21,8 +20,8 @@ L.mytrack = function(options) {return new L.Mytrack(options);}; //L.mytrack = ne
 			
 			return img;
 		},
-		_download: function(ev) {  //console.log(this._map._layers)  //must be named mytrack!
-		if('msSaveOrOpenBlob' in navigator) navigator.msSaveOrOpenBlob(new Blob([JSON.stringify(mytrack.gpx)]),"mytrack.json");  //L.mytrack???
+		_download: function(ev) {  //must be named mytrack!
+		if('msSaveOrOpenBlob' in navigator) navigator.msSaveOrOpenBlob(new Blob([JSON.stringify(mytrack.gpx)]),"mytrack.json");  //L.mytrack?
 		else ev.target.href="data:application/geo+json,"+JSON.stringify(mytrack.gpx)   
 		}
 		//, onRemove: function(map) { }   // Nothing to do here
